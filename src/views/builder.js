7768e1980e843beab42a8fc9176c78e4096619ea
Morgan Metz <mlmi4@k12albemarle.org>
2022-12-15 17:14:15 -0500

    Update builder.js

--- src/views/builder.js
+++ src/views/builder.js
@@ -1,68 +1,135 @@
-import React from 'react'
+import React from 'react';
 
-import { Helmet } from 'react-helmet'
+class Builder extends React.Component {
+  // Set initial state
+  state = {
+    input1: '',
+    input2: '',
+    input3: '',
+    input4: '',
+  };
 
-import './builder.css'
+  // Method to handle input changes and update state
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-function add() {
-  var message = document.getElementById("message").value
-  var emoji = document.getElementById("emoji").value
-  var role = document.getElementById("role").value
-  var data = document.getElementById("datatest").innerText
-  data.concat(message, emoji, role)
-  console.log(data)
-}
+  // Method to handle form submission and convert state to JSON
+  handleSubmit = event => {
 
-const Builder = (props) => {
-  return (
-    <div className="builder-container">
-      <Helmet>
-        <title>builder - Copperhead Software</title>
-        <meta property="og:title" content="builder - Copperhead Software" />
-      </Helmet>
-      <style>
-        {`
-        body {
-          background-color: #f1f1f1;
+    event.preventDefault();
+    const inputs = this.state;
+    const inputsAsJson = `{<br>` + this.state.input1 + ": {<br>" + this.state.input2 + ":{<br>" + this.state.input3 + ":" + this.state.input4 +`<br>}<br>}<br>}`;
+    // Do something with the JSON, such as displaying it in a div
+    document.getElementById('json-output').innerHTML = inputsAsJson;
+  };
+
+  render() {
+    return (
+      <div>
+        <style>
+          {`
+          body {
+            background-color: #282c34;
+            color: white;
+            font-family: 'Courier New', Courier, monospace;
+            font-size: 1.5rem;
+            text-align: left;
+          }
+          form {
+            text-align: left;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+          label {
+            margin: 1rem;
+          }
+          input {
+            margin: 0.5rem;
+          }
+          button {
+            margin: 1rem;
+            padding: 0.5rem;
+            background-color: #61dafb;
+            border: none;
+            border-radius: 0.5rem;
+            font-size: 1rem;
+          }
+          button:hover {
+            background-color: #61dafb;
+            cursor: pointer;
+          }
+          #json-output {
+            margin: 1rem;
+            padding: 1rem;
+            background-color: #282c34;
+            color: white;
+            font-family: 'Courier New', Courier, monospace;
+            font-size: 1.5rem;
+            text-align: left;
+          }
+          footer {
+            padding: 7rem;
+            display: table;
+            text-align: center;
+            margin-left: auto;
+            margin-right: auto;
         }
-        button {
-          background-color: #4CAF50;
-          border: none;
-          color: white;
-          padding: 15px 32px;
-          text-align: center;
-          text-decoration: none;
-          display: inline-block;
-          font-size: 16px;
-          pointer
-        }
-        data {
-          background-color: #ffffff;
-        }
-        `}
-      </style>
-        <h1>Reaction Role config builder</h1>
-        <form>
-          <label>Message ID:</label><br/>
-          <input type="text" id="message" name="message"/><br/>
-          <label>Emoji:</label><br/>
-          <input type="text" id="emoji" name="emoji"/><br/>
-          <label>Role ID:</label><br/>
-          <input type="text" id="role" name="role"/>
+          `}
+        </style>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Guild ID:
+            <input
+              type="text"
+              name="input1"
+              value={this.state.input1}
+              placeholder='123456789'
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Message ID:
+            <input
+              type="text"
+              name="input2"
+              placeholder='9723427836952345'
+              value={this.state.input2}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Emoji:
+            <input
+              type="text"
+              name="input3"
+              placeholder='👍'
+              value={this.state.input3}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Role ID:
+            <input
+              type="text"
+              name="input4"
+              placeholder='123456789'
+              value={this.state.input4}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+          <br />
+          <div id="json-output"></div>
         </form>
-        <button onClick={add}>Add</button>
-        <br/>
-        <br/>
-        <br/>
-        <label>Data:</label><br/>
-        <div id="datatest">
-          test
-          <p>
-            test
-          </p>
-        </div>
-    </div>
-  )
+        <footer>
+          Made with ❤️ by Copperhead Software and Chat GPT-3
+        </footer>
+      </div>
+    );
+  }
 }
 
-export default Builder
+export default Builder;
